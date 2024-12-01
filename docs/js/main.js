d52427c80e0c419c45ac423c7e741285ccb92ede
Artyushenko Daniil <foxones80@gmail.com>
2024-12-01 20:09:22 +0600

    added contacts

--- docs/js/main.js
+++ docs/js/main.js
@@ -9729,105 +9729,308 @@ mapBack?.addEventListener('click', e => {
 });
 
 // Orbit
-const orbitActive = document.querySelectorAll('.orbit__planet--active .orbit__img');
-const orbitNext = document.querySelectorAll('.orbit__planet--next .orbit__img');
-gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(orbitActive, {
-  zIndex: -1,
-  opacity: 0
-});
-gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(orbitNext, {
-  zIndex: -1,
-  opacity: 0
-});
-const nextBtn = document.querySelector('.orbit-control__btn--next');
-const prevBtn = document.querySelector('.orbit-control__btn--prev');
-function initOrbit() {
-  let currentIndex = 0;
-  const tl1 = gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.timeline();
-  const tl2 = gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.timeline();
-  function incr() {
-    if (currentIndex + 1 != orbitActive.length) {
-      currentIndex = currentIndex + 1;
-    }
-  }
-  function decr() {
-    if (currentIndex - 1 >= 0) {
-      currentIndex = currentIndex - 1;
-    }
-  }
-  function incrSlides() {
-    let prev = currentIndex - 1;
-    gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.to('.orbit', {
-      rotateZ: () => 180 * currentIndex,
-      duration: 1
+
+const contactsInitCont = document.querySelectorAll('.contacts-ways__flex');
+if (contactsInitCont && contactsInitCont.length > 0) {
+  const orbitTriggers = document.querySelectorAll('[data-cont-trigger]');
+  orbitTriggers.forEach(trigger => {
+    trigger.addEventListener('click', e => {
+      e.preventDefault();
+      const dataset = trigger.dataset.contTrigger;
+      const curr = document.querySelector(`[data-way="${dataset}"]`);
+      contactsInitCont.forEach(el => el.classList.remove('active'));
+      curr.classList.add('active');
     });
-    tl1.to(orbitActive[prev], {
-      zIndex: -1,
-      rotateZ: () => -180 * currentIndex
-    }).to('.orbit__planet--active', {
-      scale: () => !!(currentIndex % 2) ? '0.2' : '1'
-    }, "-=0.5");
-    tl1.to(orbitActive[currentIndex + 2], {
-      zIndex: currentIndex,
-      opacity: 1,
-      rotateZ: () => -180 * currentIndex
-    }, "-=0.7");
-    tl2.to(orbitNext[prev], {
-      zIndex: -1,
-      opacity: 0,
-      rotateZ: () => -180 * currentIndex
-    }).to('.orbit__planet--next', {
-      scale: () => !!(currentIndex % 2) ? '1' : '0.2'
-    }, "-=0.5");
-    tl2.to(orbitNext[(currentIndex + 2) % orbitActive.length], {
-      zIndex: currentIndex,
-      opacity: 1,
-      rotateZ: () => -180 * currentIndex
-    }, "-=0.7");
-  }
-  function decrSlides() {
-    let next = currentIndex + 1;
-    tl1.to(orbitActive[next], {
+  });
+  function initOrbit(container) {
+    let currentIndex = 0;
+    let turn = 0;
+    let rotation = 0;
+    let even = true;
+    const orbitActive = container.querySelectorAll('.orbit__planet--active .orbit__img');
+    const orbitNext = container.querySelectorAll('.orbit__planet--next .orbit__img');
+    const orbitCounter = container.querySelector('.orbit__counter');
+    const orbitActivePlanet = container.querySelector('.orbit__planet--active');
+    const orbitActiveNext = container.querySelector('.orbit__planet--next');
+    gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(orbitActive, {
       zIndex: -1,
       opacity: 0
     });
-    tl1.to(orbitActive[currentIndex], {
-      zIndex: 2,
-      opacity: 1
-    });
-    tl2.to(orbitNext[next + 1], {
+    gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(orbitNext, {
       zIndex: -1,
       opacity: 0
     });
-    tl2.to(orbitNext[(currentIndex + 1) % orbitActive.length], {
+    const nextBtn = container.querySelector('.orbit-control__btn--next');
+    const prevBtn = container.querySelector('.orbit-control__btn--prev');
+    const orbitNav = container.querySelectorAll('.contacts-ways__nav li');
+    const tl1 = gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.timeline();
+    function setOrbitCounter() {
+      orbitCounter.textContent = currentIndex < 10 ? `0${currentIndex + 1}` : `${currentIndex + 1}`;
+    }
+    function changeSlideFw(idx) {
+      const prev = currentIndex;
+      turn++;
+      gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.to(container.querySelector('.orbit__wrapper'), {
+        rotateZ: () => rotation += 180,
+        duration: 1
+      });
+      gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(container.querySelectorAll('.orbit__planet--active .orbit__img'), {
+        zIndex: -2,
+        opacity: 0
+      });
+      gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(container.querySelectorAll('.orbit__planet--next .orbit__img'), {
+        zIndex: -2,
+        opacity: 0
+      });
+      gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.to(orbitActivePlanet, {
+        rotateZ: () => turn % 2 ? '180' : '0',
+        scale: () => turn % 2 ? '0.2' : '1',
+        onUpdate: () => turn % 2 ? orbitActivePlanet.classList.add('hide') : orbitActivePlanet.classList.remove('hide')
+      });
+      gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.to(orbitActiveNext, {
+        rotateZ: () => turn % 2 ? '180' : '0',
+        scale: () => !(turn % 2) ? '0.2' : '1',
+        onUpdate: () => !(turn % 2) ? orbitActiveNext.classList.add('hide') : orbitActiveNext.classList.remove('hide')
+      });
+      if (turn % 2) {
+        let nextActiveSlide = orbitActive[idx + 1];
+        if (!nextActiveSlide) nextActiveSlide = orbitActive[idx];
+        tl1.to(nextActiveSlide, {
+          zIndex: 4,
+          opacity: 1
+        });
+        tl1.to(orbitNext[idx], {
+          zIndex: 4,
+          opacity: 1
+        }, '-=0.5');
+      } else {
+        let nextNextSlide = orbitNext[idx + 1];
+        if (!nextNextSlide) nextNextSlide = orbitNext[idx];
+        tl1.to(nextNextSlide, {
+          zIndex: 4,
+          opacity: 1
+        }, "-=0.5");
+        tl1.to(orbitActive[idx], {
+          zIndex: 4,
+          opacity: 1
+        }, "-=0.5");
+      }
+    }
+    function changeSlideBack(idx) {
+      const prev = currentIndex;
+      turn++;
+      gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.to(container.querySelector('.orbit__wrapper'), {
+        rotateZ: () => rotation -= 180,
+        duration: 1
+      });
+      gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(container.querySelectorAll('.orbit__planet--active .orbit__img'), {
+        zIndex: -2,
+        opacity: 0
+      });
+      gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(container.querySelectorAll('.orbit__planet--next .orbit__img'), {
+        zIndex: -2,
+        opacity: 0
+      });
+      gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.to(orbitActivePlanet, {
+        rotateZ: () => turn % 2 ? '180' : '0',
+        scale: () => turn % 2 ? '0.2' : '1',
+        onUpdate: () => turn % 2 ? orbitActivePlanet.classList.add('hide') : orbitActivePlanet.classList.remove('hide')
+      });
+      gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.to(orbitActiveNext, {
+        rotateZ: () => turn % 2 ? '180' : '0',
+        scale: () => !(turn % 2) ? '0.2' : '1',
+        onUpdate: () => !(turn % 2) ? orbitActiveNext.classList.add('hide') : orbitActiveNext.classList.remove('hide')
+      });
+      if (turn % 2) {
+        tl1.to(orbitActive[idx + 1], {
+          zIndex: 4,
+          opacity: 1
+        });
+        tl1.to(orbitNext[idx], {
+          zIndex: 4,
+          opacity: 1
+        }, "-=0.5");
+      } else {
+        tl1.to(orbitNext[idx + 1], {
+          zIndex: 4,
+          opacity: 1
+        });
+        tl1.to(orbitActive[idx], {
+          zIndex: 4,
+          opacity: 1
+        }, "-=0.5");
+      }
+    }
+    function clearOrbitNav() {
+      orbitNav.forEach(el => el.classList.remove('active'));
+    }
+    function setNavActive(newIdx) {
+      clearOrbitNav();
+      orbitNav[newIdx].classList.add('active');
+    }
+    function initOrbitNav() {
+      let prevItem = -1;
+      orbitNav.forEach((item, index) => {
+        item.addEventListener('click', e => {
+          e.preventDefault();
+          if (index != prevItem) {
+            clearOrbitNav();
+            item.classList.add('active');
+            even = currentIndex % 2;
+            if (index > prevItem) {
+              changeSlideFw(index);
+            } else {
+              changeSlideBack(index);
+            }
+            currentIndex = index;
+            prevItem = index;
+            setOrbitCounter(currentIndex);
+          }
+        });
+      });
+    }
+    function incr() {
+      if (currentIndex + 1 < orbitActive.length) {
+        currentIndex++;
+        return currentIndex % 2;
+      }
+    }
+    function decr() {
+      if (currentIndex - 1 >= 0) {
+        currentIndex--;
+        return currentIndex % 2;
+      }
+    }
+
+    // function incrSlides(){
+
+    //   gsap.to('.orbit', {rotateZ: () => rotation += 180, duration: 1})
+
+    //   gsap.to('.orbit__planet--active', {
+    //     rotateZ: () => currentIndex % 2 ? '180' : '0',
+    //     scale: () =>  currentIndex % 2 ? '0.2' : '1'
+    //   })
+    //   gsap.to('.orbit__planet--next', {
+    //     rotateZ: () => currentIndex % 2 ? '180' : '0',
+    //     scale: () =>  !(currentIndex % 2) ? '0.2' : '1'
+    //   })
+
+    //   if(even){
+    //     // PREV
+    //     tl1.to(orbitActive[currentIndex-1], {
+    //       zIndex: -2
+    //     })
+    //     // CURRENT
+    //     tl1.to(orbitActive[currentIndex+1], {
+    //       zIndex: 4,
+    //       opacity: 1
+    //     })
+    //   } else{
+    //     tl1.to(orbitNext[currentIndex-1], {
+    //       zIndex: -2
+    //     })
+    //     // CURRENT
+    //     tl1.to(orbitNext[currentIndex+1], {
+    //       zIndex: 4,
+    //       opacity: 1
+    //     })
+    //   }
+    // }
+
+    // function decrSlides(){
+    //   let next = currentIndex+1
+
+    //   tl1.to(orbitActive[next], {zIndex: -1, opacity: 0})
+    //   tl1.to(orbitActive[currentIndex], {zIndex: 2, opacity: 1})
+
+    //   tl2.to(orbitNext[next + 1], {zIndex: -1, opacity: 0})
+    //   tl2.to(orbitNext[(currentIndex + 1) % orbitActive.length], {zIndex: 2, opacity: 1})
+    // }
+
+    // function decrSlides(){
+    //   gsap.to('.orbit', {rotateZ: () => rotation -= 180, duration: 1})
+
+    //   gsap.to('.orbit__planet--active', {
+    //     rotateZ: () => currentIndex % 2 ? '180' : '0',
+    //     scale: () =>  currentIndex % 2 ? '0.2' : '1'
+    //   })
+    //   gsap.to('.orbit__planet--next', {
+    //     rotateZ: () => currentIndex % 2 ? '180' : '0',
+    //     scale: () =>  !(currentIndex % 2) ? '0.2' : '1'
+    //   })
+
+    //   if(!even){
+    //     // PREV
+    //     console.log(currentIndex)
+    //     tl1.to(orbitActive[currentIndex+2], {
+    //       zIndex: -2,
+    //       opacity: 0
+    //     })
+    //     // CURRENT
+    //     tl1.to(orbitActive[currentIndex], {
+    //       zIndex: 4,
+    //       opacity: 1
+    //     })
+    //   } else{
+    //     tl1.to(orbitNext[currentIndex+2], {
+    //       zIndex: -2,
+    //       opacity: 0
+    //     })
+    //     // CURRENT
+    //     tl1.to(orbitNext[currentIndex], {
+    //       zIndex: 4,
+    //       opacity: 1
+    //     })
+    //   }
+    // }
+
+    // function nextPlanet(){
+    //   if(currentIndex != orbitActive.length - 1){
+    //     even = incr()
+    //     incrSlides()
+    //   }
+
+    // }
+    // function prevPlanet(){
+    //   if(currentIndex-1 >= 0){
+    //     even = decr()
+    //     decrSlides()
+    //   }
+    // }
+    gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(orbitActive[currentIndex], {
       zIndex: 2,
       opacity: 1
     });
-  }
-  function nextPlanet() {
-    if (currentIndex != orbitActive.length - 1) {
-      incr();
-      incrSlides();
+    gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(orbitNext[currentIndex + 1], {
+      zIndex: 2,
+      opacity: 1
+    });
+    function disableBtn() {
+      if (currentIndex == orbitActive.length - 1) nextBtn.classList.add('disable');else nextBtn.classList.remove('disable');
+      if (currentIndex == 0) prevBtn.classList.add('disable');else prevBtn.classList.remove('disable');
     }
+    nextBtn.addEventListener('click', e => {
+      even = incr();
+      disableBtn();
+      changeSlideFw(currentIndex);
+      setNavActive(currentIndex);
+      setOrbitCounter(currentIndex);
+    });
+    prevBtn.addEventListener('click', () => {
+      even = decr();
+      disableBtn();
+      changeSlideBack(currentIndex);
+      setNavActive(currentIndex);
+      setOrbitCounter(currentIndex);
+    });
+    disableBtn();
+    setOrbitCounter();
+    initOrbitNav();
   }
-  function prevPlanet() {
-    if (currentIndex - 1 >= 0) {
-      decr();
-      decrSlides();
-    }
-  }
-  gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(orbitActive[currentIndex], {
-    zIndex: 2,
-    opacity: 1
+  contactsInitCont.forEach(cont => {
+    initOrbit(cont);
   });
-  gsap__WEBPACK_IMPORTED_MODULE_1__.gsap.set(orbitNext[currentIndex + 1], {
-    zIndex: 2,
-    opacity: 1
-  });
-  nextBtn.addEventListener('click', nextPlanet);
-  prevBtn.addEventListener('click', prevPlanet);
 }
-initOrbit();
 
 
 /***/ }),
